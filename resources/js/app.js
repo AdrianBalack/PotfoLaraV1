86ed58829edfb05efaee48808b6489d6f06c03d9
AdriBal <rbalagizi2016@gmail.com>
2024-11-30 19:07:00 -0600

    LaraVersion with none starter Kit just sqlite for DB, Pest for Test and native css file for frontEnd

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-import './bootstrap';
+import "./bootstrap";
+document.addEventListener("DOMContentLoaded", () => {
+    const navLinks = document.querySelectorAll(".main-nav a");
+
+    navLinks.forEach((link) => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute("href");
+            const targetSection = document.querySelector(targetId);
+
+            targetSection.scrollIntoView({
+                behavior: "smooth",
+            });
+        });
+    });
+});
